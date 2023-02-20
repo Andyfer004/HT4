eb29bdb2106bc170a5f6e92ea3b7479a20685f91
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2023-02-19 22:26:59 -0600

    COMMIT

---
+++ untitled/src/StackUsingArrayList.java
@@ -0,0 +1,64 @@
+/**
+ *
+ */
+import java.util.ArrayList;
+
+/**
+ * @author MAAG
+ *
+ */
+/**
+ * @author Andre marroquin
+ * Gabriell paz
+ * Andy fuentes
+ * Seccion 10 estructura de datos
+ */
+public class StackUsingArrayList<T> implements IStack<T> {
+
+    private ArrayList<T> miListaInterna;
+
+    public StackUsingArrayList() {
+        miListaInterna = new ArrayList<T>();
+    }
+
+
+    /**
+     * @return int
+     * retorna el tamaño de la lista
+     */
+    @Override
+    public int count() {
+        return miListaInterna.size();
+    }
+
+
+    /**
+     * @return boolean
+     * verifica si la lista esta vacia
+     */
+    @Override
+    public boolean isEmpty() {
+        return miListaInterna.isEmpty();
+    }
+
+
+    /**
+     * @param value
+     * agrega un valor a la lista
+     */
+    @Override
+    public void push(T value) {
+        miListaInterna.add(0, value);
+    }
+
+
+    /**
+     * @return T
+     * retorna el valor de la lista
+     */
+    @Override
+    public T pull() {
+        return miListaInterna.remove(0);
+    }
+
+}
